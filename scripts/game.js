3f0c3a5ad512f158e787091a9d29022b9f11b707
Lauren Hetherington <laurenlhetherington@gmail.com>
2020-01-23 13:39:33 -0500

    Start on tile generation and styling.

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,13 @@
 // javascript will go here
+const dimensions = [28, 36];
+const totalTiles = dimensions[0] * dimensions[1];
+const gameBoard = document.getElementById('game');
+
+const tiles = [];
+
+for (let i=0; i<totalTiles; i++) {
+    const newTile = document.createElement('div');
+    newTile.className = 'tile';
+    tiles.push(newTile);
+    gameBoard.appendChild(newTile);
+}
